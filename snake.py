16edca4104cba817254e1e02dfede11de4901688
Sid-Hughes <58391103+Sid-Hughes@users.noreply.github.com>
2024-10-12 22:37:04 +0100

    Change snake made into its own class

--- snake.py
+++ snake.py
@@ -1,9 +1,25 @@
 import pygame
-
 pygame.init()
 dis = pygame.display.set_mode((800, 600))
-
 pygame.display.set_caption('Snake game by Edureka')
+
+class snake:
+    def __init__(self):
+        self.block_width = 10
+        self.block_height = 10
+        self.head = pygame.Rect(dis.get_width()/2, dis.get_height()/2, 10, 10)
+
+    def move_up(self):
+        self.head.y -= 10
+    def move_down(self):
+        self.head.y += 10
+    def move_left(self):
+        self.head.x -= 10
+    def move_right(self):
+        self.head.x += 10
+
+
+
 
 white = (255, 255, 255)
 blue = (0, 0, 255)
@@ -12,7 +28,7 @@ red = (255, 0, 0)
 x1 = 400
 y1 = 300
 
-snake_head = pygame.Rect(x1, y1, 10, 10)
+snake = snake()
 
 clock = pygame.time.Clock()
 pygame.key.set_repeat(1, 30)
@@ -27,18 +43,18 @@ while not game_over:
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            snake_head.move_ip(0, -10)
+            snake.move_up()
         if keys[pygame.K_DOWN]:
-            snake_head.move_ip(0, 10)
+            snake.move_down()
         if keys[pygame.K_LEFT]:
-            snake_head.move_ip(-10, 0)
+            snake.move_left()
         if keys[pygame.K_RIGHT]:
-            snake_head.move_ip(10, 0)
+            snake.move_right()
         dis.fill(white)
-        pygame.draw.rect(dis, blue, snake_head)
+        pygame.draw.rect(dis, blue, snake.head)
         pygame.display.update()
 
-
         clock.tick(30)
+
 pygame.quit()
 quit()
